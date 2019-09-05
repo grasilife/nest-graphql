bf2c64df872b547997ee88a478fc195ec25914a6
grasilife <fu.guo.qiang@qq.com>
2019-09-06 00:32:35 +0800

    添加graphql

---
+++ src/cats/cats.resolver.ts
@@ -0,0 +1,19 @@
+﻿import { Resolver, Query } from '@nestjs/graphql';
+@Resolver()
+export class CatsResolver {
+  //   constructor(
+  //     private readonly authorsService: AuthorsService,
+  //     private readonly postsService: PostsService,
+  //   ) {}
+
+  @Query(() => String)
+  async hello() {
+    return 'hello';
+  }
+
+  //   @ResolveProperty()
+  //   async posts(@Parent() author) {
+  //     const { id } = author;
+  //     return await this.postsService.findAll({ authorId: id });
+  //   }
+}
